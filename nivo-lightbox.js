d12cb9d3508b5d25e374c392f26698fa54cb490c
Adam Cupiał <adam.cupial@codepill.com>
2014-02-24 13:27:47 +0100

    [FIXED] :: youtube playing error on ie 10+

--- nivo-lightbox.js
+++ nivo-lightbox.js
@@ -161,11 +161,11 @@
 	        		classTerm = 'nivo-lightbox-video';
 
 	        	if(video[1] == 'youtube'){
-		        	src = 'http://www.youtube.com/v/'+ video[4];
+		        	src = 'http://www.youtube.com/embed/'+ video[4];
 			        classTerm = 'nivo-lightbox-youtube';
 	        	}
 	        	if(video[1] == 'youtu'){
-		        	src = 'http://www.youtube.com/v/'+ video[3];
+		        	src = 'http://www.youtube.com/embed/'+ video[3];
 			        classTerm = 'nivo-lightbox-youtube';
 	        	}
 	        	if(video[1] == 'vimeo'){
